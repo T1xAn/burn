58ca328bd8c73c26f1c66151f8fbe8855d4fb331
T1xAn <pyps47.pyps47@yandex.ru>
2022-01-30 20:27:02 +0300

    prv

--- burn/burn.cpp
+++ burn/burn.cpp
@@ -1,5 +1,4 @@
-﻿
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <conio.h>
@@ -15,64 +14,6 @@ struct line {
 	double time;
 };
 
-//void trash() {
-//int *size = new int;
-//	cout << endl << "Enter match num - ";
-//	cin >> *size ;
-//
-//	vector <int> *arr = new vector <int> [*size];
-//	/*for (int count = 0; count < *size; ++count)
-//		arr[count] = new int[*size];*/
-//	//vector <int> arr(*size);
-//	int min = -1;
-//	int max = 1;
-//	for (int row = 0; row < *size; row++) {
-//		int min = -1;
-//			int x1 = min + rand()%((max + 1) - min); // для у ранжомить число от -1 до 1 и соответсвенно x прибавлять полученное число
-//
-//			//int buff = rand() % 3 - 1;
-//
-//
-//
-//			int Yzero = x1;
-//			int y1 = (Yzero-1) + rand()%((Yzero)-(Yzero - 1)); // записывать напрямую в структуру и при проверке сравнивать сразу начало и конец спички
-//			while (1) {
-//				int x2 = min + rand() % ((max + 1) - min);
-//				int Yzero = x2;
-//				int y2 = (Yzero - 1) + rand() % ((Yzero)-(Yzero - 1));
-//			}
-//			//int time = rand()%200;
-//			arr[row].push_back(x1);
-//			arr[row].push_back(y1);
-//			//arr[row].push_back(time);
-//			for(int i= 0; i< 2 ;i++)
-//			cout << arr[row].at(i) << " ";
-//			cout << endl;
-//			//найти минимум и максимум у X и Y, если они обновились, то увеличить или уменьшить максимум на 1
-//	}
-//
-//	/*	vector <int> a = {12, 21};
-//		vector <int> b = { 12, 21 };
-//		if (a == b) {
-//			cout << "cunt";
-//				return;
-//		}
-//		cout << "Красава";*/
-//
-//	struct line** match = new line * [*size];
-//	for (int count = 0; count < *size; ++count)
-//		match[count] = new line;
-//
-//	int buff;
-//	for (int i = 0; i < *size; i++) {
-//		for (int j = 0; j < 5; j++) {
-//			//fin >> buff;
-//			match[0]->s.push_back(buff);
-//		}
-//	}
-//}
-//
- 
 void main() {
 	ifstream fin("f.in");
 	if (!fin.is_open()) {
@@ -127,17 +68,9 @@ void main() {
 		line_num++;
 	}
 		fin.close();
-
-	/*for (int i = 0; i < size * 2; i++) {
-		cout << hline[i]->s << " "  << hline[i]->e << " " <<  hline[i]->time << endl;
-	}*/
-
-	double ** arr = new double * [points.size()]; //размеры могут быть другие
+	double ** arr = new double * [points.size()];
 	for (int count = 0; count < points.size(); ++count)
 		arr[count] = new double[points.size()];
-
-	//double arr[6][6];
-
 	for (int i = 0; i < points.size(); i++) {
 		for (int j = 0; j < points.size(); j++) {
 			if (i == j)
@@ -146,79 +79,48 @@ void main() {
 				arr[i][j] = inf;
 		}
 	}
-		// со stop тоже работает, но хз быстрее или нет
 
 	for (int count = 0; count < points.size(); count++) {
 		for (int i = 0; i < points.size(); i++) {
-			//bool stop = false;
+			bool stop = false;
 			for (int j = 0; j < line_num; j++) {
-				if (arr[count][hline[j]->s] + hline[j]->time < arr[count][hline[j]->e])
+				if (arr[count][hline[j]->s] + hline[j]->time < arr[count][hline[j]->e]) {
 					arr[count][hline[j]->e] = arr[count][hline[j]->s] + hline[j]->time;
-				if (arr[count][hline[j]->e] + hline[j]->time < arr[count][hline[j]->s])
+					stop = true;
+				}
+				if (arr[count][hline[j]->e] + hline[j]->time < arr[count][hline[j]->s]) {
 					arr[count][hline[j]->s] = arr[count][hline[j]->e] + hline[j]->time;
-				//stop = true;
+					stop = true;
+				}
 			}
-			//if (!stop) break;
+			if (!stop) break;
 		}
 	}
 
-	//for (int i = 0; i < size * 2; i++) {
-	//	for (int j = 0; j < size * 2; j++) {
-	//		cout << arr[i][j] << " ";
-	//	}
-	//	cout << endl;
-	//}
-
-	// всё ещё кусок говнокода
 	int cords;
-	double max = 0; 
-	double min = inf;
+	double max = 0 , min = inf; 
 	for (int count = 0; count < points.size(); count++) {
 		int i = 0;
 		if (points[count].first % 2 == 0 && points[count].second % 2 == 0) {
-			cords = count;
+			/*cords = count;*/
 			max = 0;
 			for (i ; i < line_num; i++) {
 				double time = hline[i]->time;
 				double ts = arr[count][hline[i]->s];
 				double te = arr[count][hline[i]->e];
 				if (ts > te) swap(ts, te);
-				//
-				//if (ts - te == 0) 
-				//	time = time / 2;
-				//if(te-ts != 0)
-				//	time = ts + (time - te - ts) / 2;
-				//else if (te == ts + time) 
-				//	time = te;  //continue;
-
-
-				if (ts - te == 0) time = time / 2;
-			    if (te == ts + time) time = te;  //continue;
-				else time = ts +(te-ts) + (time - (ts + (te - ts)))/ 2;
+				
+				time = ts + (te - ts) + (time - (te - ts)) / 2;
+				//if (ts - te == 0) time = time / 2;
+			 //   if (te == ts + time) time = te;  //continue;
+				//else time = ts + (te-ts) + (time - (te - ts))/ 2;
 				if (time > max) max = time;
 			}
-			if (min >= max) min = max; cords = count;
+			if (min > max) { min = max; cords = count; }
 		}
 
 	 }
 	ofstream fon("f.out");
-	fon << points[cords].first / 2 << " " << points[cords].second /2 << endl << max;
+	fon << points[cords].first / 2 << " " << points[cords].second /2 << endl << min;
 	fon.close();
-
-
-	
-	/*int** arr = new int* [size];
-	for (int count = 0; count < size; ++count)
-		arr[count] = new int[5];
-
-	cout << endl << size << endl;
-
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < 5; j++) {
-			fin >> arr[i][j];
-			cout << arr[i][j] << " ";
-		}
-		cout << endl;
-	}
-	*/
 }
