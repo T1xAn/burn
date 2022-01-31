f3e6117e2f9858e622e5aa6a549091ec6fd16c19
T1xAn <pyps47.pyps47@yandex.ru>
2022-01-31 15:59:55 +0300

    fin

--- burn/burn.cpp
+++ burn/burn.cpp
@@ -1,13 +1,9 @@
-﻿#include <stdio.h>
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
-#include <conio.h>
 #include <fstream>
 #include <iomanip>
-
 using namespace std;
 #define inf 1e8
-	
 struct line {
 	int s;
 	int e;
@@ -20,36 +16,25 @@ void main() {
 		cout << endl << "File can not be opend";
 		return;
 	}
-
 	int size;
 	fin >> size;
-
 	struct line** hline = new line * [size*2];
 		for (int count = 0; count < size*2; ++count)
 			hline[count] = new line;
-	
-		int line_num = 0;
-		vector <pair <int, int>> points;
-
+	int line_num = 0;
+	vector <pair <int, int>> points;
 		for (int i = 0; i < size; i++) {
 			int x1, y1, x2, y2, xm, ym;
-			float t;
-
+			double t;
 			fin >> x1 >> y1 >> x2 >> y2 >> t;
-			x1 *= 2;
-			x2 *= 2;
-			y1 *= 2;
-			y2 *= 2;
-			t /= 2;
-
+			x1 *= 2; x2 *= 2; y1 *= 2; y2 *= 2; t /= 2;
 			xm = (x1 + x2) / 2;
 			ym = (y1 + y2) / 2;
 
 			int p, p1, p2, pm;
-			
 			p = find(points.begin(), points.end(), make_pair(x2, y2)) - points.begin();
-				if (p == points.size()) points.push_back({ x2,y2 });
-			p2 = p;
+			if (p == points.size())  points.push_back({ x2,y2 }); 
+			p2 = p; 
 			p = find(points.begin(), points.end(), make_pair(xm, ym)) - points.begin();
 				if (p == points.size()) points.push_back({ xm,ym });
 			pm = p;
@@ -67,16 +52,15 @@ void main() {
 		hline[line_num]->time = t;
 		line_num++;
 	}
-		fin.close();
+	fin.close();
+
 	double ** arr = new double * [points.size()];
 	for (int count = 0; count < points.size(); ++count)
 		arr[count] = new double[points.size()];
 	for (int i = 0; i < points.size(); i++) {
 		for (int j = 0; j < points.size(); j++) {
-			if (i == j)
-				arr[i][j] = 0;
-			else
-				arr[i][j] = inf;
+			if (i == j) arr[i][j] = 0;
+			else arr[i][j] = inf;
 		}
 	}
 
@@ -100,27 +84,21 @@ void main() {
 	int cords;
 	double max = 0 , min = inf; 
 	for (int count = 0; count < points.size(); count++) {
-		int i = 0;
 		if (points[count].first % 2 == 0 && points[count].second % 2 == 0) {
-			/*cords = count;*/
 			max = 0;
-			for (i ; i < line_num; i++) {
+			for (int i = 0 ; i < line_num; i++) {
 				double time = hline[i]->time;
 				double ts = arr[count][hline[i]->s];
 				double te = arr[count][hline[i]->e];
 				if (ts > te) swap(ts, te);
-				
 				time = ts + (te - ts) + (time - (te - ts)) / 2;
-				//if (ts - te == 0) time = time / 2;
-			 //   if (te == ts + time) time = te;  //continue;
-				//else time = ts + (te-ts) + (time - (te - ts))/ 2;
 				if (time > max) max = time;
 			}
-			if (min > max) { min = max; cords = count; }
+			if (min >= max) { min = max; cords = count; }
 		}
-
 	 }
 	ofstream fon("f.out");
-	fon << points[cords].first / 2 << " " << points[cords].second /2 << endl << min;
+	fon.setf(ios::fixed);
+	fon << points[cords].first / 2 << " " << points[cords].second /2 << endl << setprecision(3) << min;
 	fon.close();
 }
